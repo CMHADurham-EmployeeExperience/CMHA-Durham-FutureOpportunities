84960728512348b5327a64581fe78c7344815f0f
agent-mj00 <agentzero00xx@gmail.com>
2025-06-26 15:13:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // script.js
+
 function nextPage() {
   alert("This button could link to another page!");
   // Or redirect to another page:
   // window.location.href = "nextpage.html";
-  <button onclick="goToWebsite()">Go to CMHA Durham</button>
+}
 
-  <script>
-    function goToWebsite() {
-      window.location.href = "https://docs.github.com/en/pages";
-    }
-
+// If you want to add a function to go to CMHA Durham, add it here:
+function goToWebsite() {
+  window.location.href = "https://docs.github.com/en/pages";
+}
