b76650a72b447ecee57ab3c743675d6f6220d46a
agent-mj00 <agentzero00xx@gmail.com>
2025-06-26 14:57:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // script.js
 function nextPage() {
   alert("This button could link to another page!");
-  // Or redirect to another page:
-  // window.location.href = "https://cmhadurham-futureopportunities.my.canva.site/cmhadurham";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Redirect Button</title>
+</head>
+<body>
+
+  <h1>Click the Button to Visit Our Site</h1>
+  <button onclick="goToWebsite()">Go to CMHA Durham</button>
+
+  <script>
+    function goToWebsite() {
+      window.location.href = "https://cmhadurham.ca"; // Replace with your desired URL
+    }
+  </script>
+
+</body>
+</html>
+
