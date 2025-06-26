2ec2797a8a82fa3d41a0be543f7e7e44b1e608f9
agent-mj00 <agentzero00xx@gmail.com>
2025-06-26 15:18:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // script.js
 
-function nextPage() {
-  alert("This button could link to another page!");
-  // Or redirect to another page:
-  // window.location.href = "nextpage.html";
-}
-
 // If you want to add a function to go to CMHA Durham, add it here:
 function goToWebsite() {
   window.location.href = "https://docs.github.com/en/pages";
