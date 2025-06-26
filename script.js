366bebaf487c9041fffbce348d37e6e9eea6be63
agent-mj00 <agentzero00xx@gmail.com>
2025-06-26 11:43:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 function nextPage() {
   alert("This button could link to another page!");
   // Or redirect to another page:
-  // window.location.href = "nextpage.html";
+  // window.location.href = "https://cmhadurham-futureopportunities.my.canva.site/cmhadurham";
 }
